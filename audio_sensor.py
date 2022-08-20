13597aa1962e2b9531f67e9bb047475717744af2
Stian Forseth <stiafo@stud.ntnu.no>
2022-08-20 20:09:49 +0200

    Create script to play audio when sensor is high

---
+++ audio_sensor.py
@@ -0,0 +1,33 @@
+#! /usr/bin/python
+
+import subprocess
+import random
+import time
+import atexit
+
+import RPi.GPIO as GPIO
+
+pin_number = 12
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(pin_number, GPIO.IN)
+
+def cleanup():
+    GPIO.cleanup()
+
+atexit.register(cleanup)
+
+tracks_as_string = str((subprocess.run("ls /mnt/usb0 | grep '.mp3'", shell=True, capture_output=True).stdout), "utf-8")
+tracks = tracks_as_string.split("\n")
+tracks.pop(-1)
+
+while True:
+    pin_state = GPIO.input(pin_number) # Returns 1 if HIGH and 0 if LOW
+
+    if pin_state == 1:
+        print("PIN is high")
+        track=tracks[random.randint(0, len(tracks)-1)]
+
+        subprocess.run(f"mpg123 --stereo /mnt/usb0/{track}", shell=True)
+        time.sleep(19)
+
+    time.sleep(1)

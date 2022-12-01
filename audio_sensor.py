50d6e129d82738298f65f25fee38a68844bbcdd8
Stian <stian-fo@hotmail.com>
2022-12-01 19:00:54 +0100

    Update script to handle dual sensorsUpdate script to handle dual sensors

--- audio_sensor.py
+++ audio_sensor.py
@@ -7,7 +7,8 @@ import atexit
 
 import RPi.GPIO as GPIO
 
-pin_number = 12
+pin_left = 8
+pin_right = 22
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pin_number, GPIO.IN)
 
@@ -21,13 +22,18 @@ tracks = tracks_as_string.split("\n")
 tracks.pop(-1)
 
 while True:
-    pin_state = GPIO.input(pin_number) # Returns 1 if HIGH and 0 if LOW
+    pin_state_right = GPIO.input(pin_right) # Returns 1 if HIGH and 0 if LOW
+    pin_state_left = GPIO.input(pin_left)
 
-    if pin_state == 1:
-        print("PIN is high")
-        track=tracks[random.randint(0, len(tracks)-1)]
+    if pin_state_right == 1 or pin_state_left == 1:
+        sleep(1)
 
-        subprocess.run(f"mpg123 --stereo /media/hoyby/KINGSTON/{track}", shell=True)
-        time.sleep(19)
+        # Only play track if the other sensor is not activated within a second (probably a car)
+        if not (pin_state_left == 1 and pin_state_right == 1):
+            track=tracks[random.randint(0, len(tracks)-1)]
+            subprocess.run(f"mpg123 --stereo /media/hoyby/KINGSTON/{track}", shell=True)
+            sleep(14)
+
+        sleep(5)
 
     time.sleep(1)
